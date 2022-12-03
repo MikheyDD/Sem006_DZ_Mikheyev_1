53c8a052232fdbef822b7ed5ef79aff61dff332e
Mikheyev_Denis <mikheydd@mail.ru>
2022-12-03 13:50:05 +0300

    Sem006

---
+++ task41/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Введите числа через запятую или через пробел");
+var stringNums = Console.ReadLine();
+
+string[] numsArray = stringNums.Split(' ', ',');
+int[] numsArrayConvert = ConvertNums(numsArray);
+Print(numsArrayConvert);
+Console.WriteLine();
+Console.WriteLine(GetCount(numsArrayConvert));
+
+int GetCount(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int[] ConvertNums(string[] arr)
+{
+   int[] arrayConvert = new int[arr.Length];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arrayConvert[i] = Convert.ToInt32(arr[i]);
+    }
+    return arrayConvert;
+}
+
+void Print(int[] array)
+{
+    foreach (var item in array)
+    {
+        Console.Write($"{item}, ");
+    }
+}
---
+++ task43/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 
+// задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.WriteLine("Введите число b1");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите число k1");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите число b2");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите число k2");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+GetPoint(b1, k1, b2, k2);
+
+void GetPoint(double b1, double k1, double b2, double k2)
+{
+    if (k1 != k2)
+{
+    double x = (b1 - b2) / (k2 - k1);
+    double y = k1 * x + b1;
+    Console.Write($"({x},{y})");
+}
+else
+{
+    if (k1 == 0 && k2 == 0)
+    {
+        Console.WriteLine("прямые не пересекаются");
+    }
+    else Console.WriteLine("прямые не пересекаются");
+}
+}
